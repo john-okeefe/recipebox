2b9c8766e35cb0cfe496364e47f4b42769e61072
John O'Keefe <jokeefe@fastmail.com>
2017-02-09 10:14:07 -0600

    figuring out array

--- src/components/recipe-form/recipe-form.js
+++ src/components/recipe-form/recipe-form.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 // import RecipeName from './recipe-name'
 // import IngredientsInput from './ingredients-input'
-
+let text = "";
+let stateArr = [];
 let ingredientList;
 
  class RecipeForm extends Component {
@@ -19,7 +20,6 @@ let ingredientList;
    render(){
      return (
        <form onSubmit={this.handleSumit} className="recipe-form">
-         {console.log(this.state)}
          <h2 className="add-recipe-title">Add a Recipe</h2>
          {/* draw a line here in future */}
          <div>
@@ -28,11 +28,11 @@ let ingredientList;
         </div>
          <div>
           <strong>Ingredients</strong>
-          <input name="ingredients" onChange={this.handleInputChange} value={this.state.ingredients} />
+          <input name="ingredients" onChange={this.handleInputChange} value={text} />
         </div>
          {/* draw a line here in future */}
          <div>
-           <button type="submit" value="Submit">Add Recipe</button>
+           <button type="submit" onClick={this.handleSubmit}>Add Recipe</button>
            <button>Close</button>
          </div>
        </form>
@@ -45,16 +45,27 @@ let ingredientList;
         [event.target.name]: event.target.value
       });
     }else if(event.target.name === "ingredients"){
-      this.setState({
-        [event.target.name]: 
-      })
+        text += event.target.value;
+        console.log(text)
+      }
+      // let newArray = this.state.ingredients.slice(',');
+      // newArray.push(event.target.value)
+      // this.setState({
+      //   [event.target.name]: newArray
+      // })
     }
-    console.log(event.target.value)
-  }
+  
+
 
     handleSubmit(event){
-      event.preventDefault();
+      let ingredientsArr = text.split(',');
+      this.setState({
+        ingredients: ingredientsArr
+      })
+      text = '',
       console.log(this.state)
+      // console.log(event)
+      event.preventDefault();
    }
 }
 
