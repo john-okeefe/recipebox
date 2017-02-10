01e29ade1700091efabd2a1f24e4303477a9c418
John OKeefe <nymusicman@gmail.com>
2017-02-10 23:18:12 +0000

    think I finally have it

--- src/components/recipe-form/recipe-form.js
+++ src/components/recipe-form/recipe-form.js
@@ -9,64 +9,49 @@ let ingredientList;
    constructor(props){
      super(props);
      this.state = {
-       recipe: "",
-       ingredients: []
-     }
+      recipes: [
+        {
+         id: "",
+         recipeName: "",
+         ingredients: []
+        }
+       ]
+     };
 
-     this.handleInputChange = this.handleInputChange.bind(this);
-     this.handleSubmit = this.handleSubmit.bind(this);
+     // this.handleSubmit = this.handleSubmit.bind(this);
+   }
+   
+   addRecipe(e){
+      e.preventDefault();
+      console.log(this);
+      let id = Math.floor((Math.random() * 100) + 1);
+      let recipeName = this.refs.recipeName.value;
+      let ingredients = this.refs.ingredients.value.split(',');
+      this.setState({
+       recipes: this.state.recipes.concat({id, recipeName, ingredients})
+      });
    }
 
    render(){
      return (
-       <form onSubmit={this.handleSumit} className="recipe-form">
+       <form onSubmit={this.addRecipe.bind(this)} className="recipe-form">
+       {/* Simplified form using video https://www.youtube.com/watch?v=1eNIYif69_0 */}
          <h2 className="add-recipe-title">Add a Recipe</h2>
          {/* draw a line here in future */}
          <div>
-          <strong>Recipe</strong>
-          <input name="recipe" onChange={this.handleInputChange} value={this.state.recipe} />
+          <strong>Recipe Name</strong>
+          <input ref="recipeName" placeholder="Type the Recipe Name" />
         </div>
          <div>
           <strong>Ingredients</strong>
-          <input name="ingredients" onChange={this.handleInputChange} value={text} />
+          <input ref="ingredients" placeholder="Type,the,ingredients,in,this,format" />
         </div>
          {/* draw a line here in future */}
          <div>
-           <button type="submit" onClick={this.handleSubmit}>Add Recipe</button>
+           <button type="submit">Add Recipe</button>
            <button>Close</button>
          </div>
        </form>
-     )
-   }
-
-   handleInputChange(event){
-    if(event.target.name === "recipe"){     
-      this.setState({
-        [event.target.name]: event.target.value
-      });
-    }else if(event.target.name === "ingredients"){
-        text += event.target.value;
-        console.log(text)
-      }
-      // let newArray = this.state.ingredients.slice(',');
-      // newArray.push(event.target.value)
-      // this.setState({
-      //   [event.target.name]: newArray
-      // })
-    }
-  
-
-
-    handleSubmit(event){
-      let ingredientsArr = text.split(',');
-      this.setState({
-        ingredients: ingredientsArr
-      })
-      text = '',
-      console.log(this.state)
-      // console.log(event)
-      event.preventDefault();
+     );
    }
 }
-
-export default RecipeForm;
